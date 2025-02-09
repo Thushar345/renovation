76b76d16413adba768547f0c39d054453748a008
Thushar Vinod V <thushar3013@gmail.com>
2025-02-10 01:45:24 +0530

    added get endpoint for waitlist entries

--- routes/waitlistRoutes.js
+++ routes/waitlistRoutes.js
@@ -7,11 +7,22 @@ router.post('/', async (req, res) => {
     const { name, email, semester, feature } = req.body;
 
     try {
-        const newEntry = new Waitlist({ name, email, semester, feature });
+        const newEntry = new Waitlist({ name, email, semester, feature});
         await newEntry.save();
         res.status(201).json({ message: 'Waitlist entry added successfully!' });
     } catch (error) {
         res.status(500).json({ message: 'Error adding to waitlist', error: error.message });
+    }
+});
+
+
+// Get All Waitlist Entries
+router.get('/', async (req, res) => {
+    try {
+        const waitlistEntries = await Waitlist.find();
+        res.status(200).json(waitlistEntries);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error. Please try again later.' });
     }
 });
 
